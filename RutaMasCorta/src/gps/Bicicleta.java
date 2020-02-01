6eba27385b4c64faa594b7915badb56fe4aba668
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-02-01 22:16:28 +0100

    añadido método: private static int sumador(int[] ruta)

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -19,12 +19,11 @@ public class Bicicleta {
         int sum2 = 0;
         int sum3 = 0;
         
-        for(int i=0; i<camino1.length; i++)
-        {
-            sum1 += camino1[i];
-            sum2 += camino2[i];
-            sum3 += camino3[i];
-        }
+        sum1 = sumador(camino1);
+        
+        sum2 = sumador(camino2);
+        
+        sum3 = sumador(camino3);
         
         if(sum1 < sum2 && sum1 < sum3)
         {
@@ -51,12 +50,21 @@ public class Bicicleta {
         
     }
 
+	private static int sumador(int[] ruta) {
+		int suma = 0;
+		for(int i=0; i<ruta.length; i++)
+        {
+            suma += ruta[i];
+        }
+		return suma;
+	}
+
     public static void main(String[] args) {
         Bicicleta bici = new Bicicleta();
         int[] camino1 = {12,34,21,46,25};
         int[] camino2 = {24,1,5,64,10,15,21};
         int[] camino3 = {1,5,6,7,8,10,20,46,104};
-        System.out.println("El camino mÃ¡s corto es:"+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
+        System.out.println("El camino más corto es:"+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
     }
 }
 
